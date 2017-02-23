48dfdd5eb0688799fa3fa36c847ae2855be0f3c0
Tandra Chakraborty <tandra0503877@gmail.com>
2017-02-22 22:27:44 -0500

    Update testing.java

--- testing.java
+++ testing.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Random rand=new Random();
-        int t=rand.nextInt(6);
+        int t=5;
         System.out.println(t);
         int k;
         Set<Integer> input=new HashSet<Integer>();
@@ -18,8 +18,10 @@ public class Solution {
             input.add(rand.nextInt(200-3+1)+3);
             
         }
+        int count=0;
         for(Integer n:input)
         {
+            count++;
             k=rand.nextInt(n-1+1)+1;
             System.out.println(n+" "+k);
             int ai;
@@ -28,8 +30,18 @@ public class Solution {
             for(int i=1;i<=n;i++)
             {
                 ai=rand.nextInt((max-min)+1)+min;
-                if(i%3==0){if(ai>0)ai=-1*ai;}
-                else if(i%3==1){ai=0;}
+                if(count%2==0)
+                {
+                    if(i<k && ai>0)ai=-1*ai;
+                    if(i==k)ai=0;
+                    if(i>k && ai<0)ai=ai*-1;
+                }
+                else
+                {   if(i==1)ai=0;
+                    else if(i==2)ai=-1;
+                    else if(ai<0)ai=-1*ai;
+                    
+                }
                 System.out.print(ai+" ");    
             }
             //ai=rand.nextInt((max-min)+1)+min;
