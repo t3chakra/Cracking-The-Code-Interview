e7ca4fe73d91fb6776439868f58ea6017af39399
Tandra Chakraborty <tandra0503877@gmail.com>
2017-03-29 00:14:22 -0400

    Update cutStick.py

--- cutStick.py
+++ cutStick.py
@@ -2,14 +2,13 @@ n=int(raw_input().strip())
 arr=map(int,raw_input().strip().split())
 arr=sorted(arr)
 arr.reverse()
-print n
-while len(arr)>1:
+while len(arr)>0:
+    print len(arr)
     for i in range(len(arr)):
         arr[i]=arr[i]-arr[-1]
         if arr[i]==0:
             break
     while i<len(arr):
         arr.pop()
-    print arr
-    print len(arr)
-
+    #print arr
+    #print len(arr)
